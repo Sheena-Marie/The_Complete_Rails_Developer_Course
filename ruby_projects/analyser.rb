3a435d31b3eb5e52d7d473d7fff1b5b29f940b78
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-04-16 16:24:10 +1000

    analyser.rb - ask a persons first and last name, welcome them, tell them how many characters in each, then reverse it

--- ruby_projects/analyser.rb
+++ ruby_projects/analyser.rb
@@ -11,3 +11,14 @@
 
 puts "What is your first name?"
 first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Welcome, #{first_name + last_name}!"
+
+puts "Your first name is #{first_name.length} characters long."
+
+puts "Your last name is #{last_name.length} characters long"
+
+puts "Your name reversed is #{first_name.reverse + " " + last_name.reverse}"
