ca6a60d8b1d623917705761aa03ace5141d07040
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2017-04-16 15:54:24 +1000

    adding the first files

---
+++ ruby_projects/hello_world_method.rb
@@ -0,0 +1,16 @@
+# Thsi is a method
+
+def hello
+  puts "Hello world!"
+end
+
+# This calls the method
+hello
+
+def anything(hello2)
+  puts hello2
+end
+
+anything("My name is Sheena")
+anything("Welcome to the Easter Holiday")
+anything("This will print whatever I want")
---
+++ ruby_projects/hello_world_string.rb
@@ -0,0 +1,6 @@
+# This is a variable
+hello = "Hello World!"
+puts hello
+
+hello = "Welcome to the complete Ruby on Rails developer course"
+puts hello
